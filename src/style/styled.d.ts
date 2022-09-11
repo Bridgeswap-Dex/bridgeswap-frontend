615e9fdc7d8d1c6c712198204e510bb24aa9e406
doge <110700029+tototi932@users.noreply.github.com>
2022-09-11 01:23:23 -0300

    '

--- src/style/styled.d.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from '@pancakeswap-libs/uikit/dist/theme'
-
-declare module 'styled-components' {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends PancakeTheme {}
-}
